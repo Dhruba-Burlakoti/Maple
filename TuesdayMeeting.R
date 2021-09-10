1b20e9fc92ba592d483308a918af97e342faa9ee
Dhruba-Burlakoti <57421255+Dhruba-Burlakoti@users.noreply.github.com>
2021-09-10 09:50:25 -0400

    Update TuesdayMeeting.R

--- TuesdayMeeting.R
+++ TuesdayMeeting.R
@@ -87,17 +87,116 @@ rm <- aggregate(RM$BA, list(as.factor(RM$Prd)), sum, na.rm = TRUE) # for each pe
 # write.table(rm, file = "filename", sep = ",")
 
 
+################################################################################
+# Friday First Trial #
+setwd("C:/Users/dburl\\Mapl")
+dat <- read.csv("New.csv")
+library(tidyverse)
+plot <- read.csv("PLOT.csv")
+maplestates <- filter(plot, STATECD %in% c(27, 55,19,29,5,22,17,21,47,28,1,18, 26,39,13,12,37,45,51,54,36,42,34,10,15,9,25,44,33,50,23,11))
+maplestates <- select(maplestates, CN, STATECD) # 32 states
 
-#################################################
+dat <- dat %>% left_join(maplestates, by = c("PLT_CN" = "CN"))
+dat$STATECD <- as.factor(dat$STATECD)
+
+head(dat)
+rm(maplestates, plot)
+
+# Convert data to metric units and calculate Basal Area for each trees
+dat$DBH <- dat$DIA * 2.54 # DBH inch to cm
+dat$TPH <- dat$TPA_UNADJ/0.405 # acre to hectare
+
+# Classify data into periods:
+dat$Prd <- NA
+dat$Prd[between(dat$INVYR, 1990, 1999)] <- "P1"
+dat$Prd[between(dat$INVYR, 2000, 2010)] <- "P2"
+dat$Prd[dat$INVYR>=2010] <- "P3"
+
+# Identifier for plot and year
+dat$PLT_YR <- paste0(dat$PLT_CN, "_", dat$INVYR)
+
+names(dat)
+# Calculate Key Plot level attributes:
+var <- c("PLT_YR", "INVYR", "PLT_CN", "STATUSCD", "DBH", "TPH", "LAT", "LON", "ELEV", "SPCD", "STATECD", "Prd" )
+dat1 <- dat[ , var]
+
+# Start working on Plot level attributes:
+N <- tapply(dat1$TPH, dat1$PLT_YR, sum)
+summary(N)
+
+# Calculate total number of Red Maple by plot and year:
+N_ar <- tapply(ifelse(dat1$SPCD == 316, dat1$TPH, 0), dat1$PLT_YR, sum)
+summary(N_ar)
+
+# Percentage of Red Maple:
+Perc_ar <- tapply(ifelse(dat1$SPCD == 316, dat1$TPH, 0), dat1$PLT_YR, sum)*(100/N)
+summary(Perc_ar)
+
+# Removal percentage for Red maple
+N_rem_perc_ar <- tapply(ifelse(dat1$STATUSCD == 3 & dat1$SPCD == 316, dat1$TPH, 0), dat1$PLT_YR,sum)/N_ar *100
+summary(N_rem_perc_ar)
+
+########################################
+# Calculate total stand basal area:
+
+B <- tapply(3.14*dat1$DBH^2/40000*dat1$TPH, dat1$PLT_YR, sum)
+summary(B)
+
+# Calculate total stand basal area for Red Maple
+B_ar <- tapply(ifelse(dat1$SPCD == 316, 3.14*dat1$DBH^2/40000*dat1$TPH, 0), dat1$PLT_YR, sum)
+summary(B_ar)
 
 
+# Stand basal area removal percentage for Red Maple
+B_rem_perc_ar <- tapply(ifelse(dat1$STATUSCD == 3 & dat1$SPCD == 316, 3.14*dat1$DBH^2/40000*dat1$TPH, 0), dat1$PLT_YR, sum)/B_ar *100
+summary(B_rem_perc_ar)
+
+# Plot coordinates
+Coords_y <- tapply(dat1$LAT,dat1$PLT_YR,mean)
+Coords_x <- tapply(dat1$LON,dat1$PLT_YR,mean)
+
+# Plot
+PLT <- tapply(dat1$PLT_CN, dat1$PLT_YR, mean)
+
+# Year
+YR <- tapply(dat1$INVYR, dat1$PLT_YR, mean)
 
 
+# STATE <- tapply(dat1$STATECD, dat1$PLT_YR, mean, na.rm = TRUE)
 
 
+# Merge plot-level data
+PLT_df <- cbind.data.frame(PLT, YR, Coords_x, Coords_y, N, B, N_ar, B_ar, B_rem_perc_ar,
+                           N_rem_perc_ar, STATE)
+
+
+PLT_df$PLT_YR <- paste0(PLT_df$PLT,"_",PLT_df$YR)
+
+# Remove plots where Red Maple is absent
+PLT_df1 <- PLT_df %>% drop_na(B_ar)
+PLT_df2 <- filter(PLT_df1, B_ar !=0)
+PLT_df2 <- PLT_df2 %>% drop_na(Prd)
+
+write.csv(PLT_df2, "Test.csv", row.names = FALSE)
+
+
+dat$Prd <- NA
+dat$Prd[between(dat$INVYR, 1990, 1999)] <- "P1"
+dat$Prd[between(dat$INVYR, 2000, 2010)] <- "P2"
+dat$Prd[dat$INVYR>=2010] <- "P3"
+
+
+PLT_df$Prd <- NA
+PLT_df$Prd[between(PLT_df$YR, 1990, 1999)] <- "P1"
+PLT_df$Prd[between(PLT_df$YR, 2000, 2010)] <- "P2"
+PLT_df$Prd[PLT_df$YR > 2010] <- "P3"
+
+
+PLT_df$STATE <- NULL
+
+write.csv(PLT_df, "Maple_Plots.csv", row.names = FALSE)
 
 ################################################################################
-
 
 
 
