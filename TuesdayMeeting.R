7681bacefe6bab20f8e32a6c1709f850c302dcf2
Dhruba-Burlakoti <57421255+Dhruba-Burlakoti@users.noreply.github.com>
2021-09-10 09:48:05 -0400

    Add files via upload

---
+++ TuesdayMeeting.R
@@ -0,0 +1,103 @@
+setwd("C:/Users/dburl\\Mapl")
+dat <- read.csv("New.csv")
+library(tidyverse)
+plot <- read.csv("PLOT.csv")
+maplestates <- filter(plot, STATECD %in% c(27, 55,19,29,5,22,17,21,47,28,1,18, 26,39,13,12,37,45,51,54,36,42,34,10,15,9,25,44,33,50,23,11))
+maplestates <- select(maplestates, CN, STATECD) # 32 states
+
+dat <- dat %>% left_join(maplestates, by = c("PLT_CN" = "CN"))
+dat$STATECD <- as.factor(dat$STATECD)
+
+head(dat)
+rm(maplestates, plot)
+
+# Convert data to metric units and calculate Basal Area for each trees
+dat$DBH <- dat$DIA * 2.54 # DBH inch to cm
+
+
+# Classify data into periods:
+dat$Prd <- NA
+dat$Prd[between(dat$INVYR, 1990, 1999)] <- "P1"
+dat$Prd[between(dat$INVYR, 2000, 2010)] <- "P2"
+dat$Prd[dat$INVYR>=2010] <- "P3"
+
+# Calculate basal area of each trees
+dat <- mutate(dat,
+              BA = (3.14/40000)*DBH^2 *TPH)
+
+
+# Select only required columns:
+dat1 <- select(dat, PLT_CN:SPCD, STATECD:BA)
+
+
+####################################
+# Remove data which does not belong within our designated period
+dat1 <- dat1 %>% drop_na(Prd)
+
+# Select Red Maple trees:
+RM <- filter(dat1, SPCD == 316)
+RM$SPCD <- NULL
+RM <- filter(RM, INVYR != 9999) # Remove year 9999
+
+################################################################
+# Separate object for each period:
+RM_P1 <- filter(RM, Prd == "P1")
+RM_P1$Prd <- NULL
+RM_P2 <- filter(RM, Prd == "P2")
+RM_P2$Prd <- NULL
+RM_P3 <- filter(RM, Prd == "P3")
+RM_P3$Prd <- NULL
+
+##############################################################
+# Calculate Basal Area for each State for 3 periods
+rem <- aggregate(RM_P1$BA, list(as.factor(RM_P1$STATECD)), sum, na.rm = TRUE)
+# write.table(rem, file = "RM_BA_States", sep = ",")
+# rm(rem)
+
+# Now calculate for period 2 and 3 # copy to excel
+aggregate(RM_P2$BA, list(as.factor(RM_P2$STATECD)), sum, na.rm = TRUE)
+aggregate(RM_P3$BA, list(as.factor(RM_P3$STATECD)), sum, na.rm = TRUE)
+
+
+###############################################################
+# Calculate removal basal area for each state for 3 periods
+RM_rem1 <- filter(RM_P1, STATUSCD == 3)
+RM_rem1$STATUSCD <- NULL
+RM_rem2 <- filter(RM_P2, STATUSCD == 3)
+RM_rem2$STATUSCD <- NULL
+RM_rem3 <- filter(RM_P3, STATUSCD == 3)
+RM_rem3$STATUSCD <- NULL
+
+# Calculate Removal basal area for each state for 3 periods
+aggregate(RM_rem1$BA, list(as.factor(RM_rem1$STATECD)), sum, na.rm = TRUE)
+aggregate(RM_rem2$BA, list(as.factor(RM_rem2$STATECD)), sum, na.rm = TRUE)
+aggregate(RM_rem3$BA, list(as.factor(RM_rem3$STATECD)), sum, na.rm = TRUE)
+
+
+
+
+## Calculate Sum of Basal Area for each inventory year ###
+levels(as.factor(RM$INVYR))
+tapply(RM$BA, as.factor(RM$INVYR), sum, na.rm = TRUE)
+aggregate(RM$BA, list(as.factor(RM$INVYR)), sum, na.rm = TRUE)
+
+
+# tapply(RM$BA, as.factor(RM$Prd), sum, na.rm = TRUE)
+rm <- aggregate(RM$BA, list(as.factor(RM$Prd)), sum, na.rm = TRUE) # for each period
+# write.table(rm, file = "filename", sep = ",")
+
+
+
+#################################################
+
+
+
+
+
+
+
+################################################################################
+
+
+
+
